54292ac4107787c809da3b28f57a253665759ad4
Hairil Hasyimi Omar <115607157+hairil-ironsoftware@users.noreply.github.com>
2022-11-28 16:46:34 +0700

    Update GetNumberOfFramesInAnyBitmap.cs

--- IronDrawingExamples/AnyBitmap Example/GetNumberOfFramesInAnyBitmap.cs
+++ IronDrawingExamples/AnyBitmap Example/GetNumberOfFramesInAnyBitmap.cs
@@ -1,6 +1,7 @@
 ﻿using IronSoftware.Drawing;
 using System;
 
+// Get and print number of frame in any loaded image
 var multiFrameImage = AnyBitmap.FromFile("FILE_PATH.gif");
 var frameNumber = multiFrameImage.FrameCount;
-Console.WriteLine(frameNumber); //Get number of frame in any loaded image
+Console.WriteLine(frameNumber); 
