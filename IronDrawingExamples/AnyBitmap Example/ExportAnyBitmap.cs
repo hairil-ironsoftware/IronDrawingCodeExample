446e7c6c0db8b9b1cd33741c0b2862f91fb66c2e
Hairil Hasyimi Omar <115607157+hairil-ironsoftware@users.noreply.github.com>
2022-11-28 16:37:24 +0700

    Update ExportAnyBitmap.cs

--- IronDrawingExamples/AnyBitmap Example/ExportAnyBitmap.cs
+++ IronDrawingExamples/AnyBitmap Example/ExportAnyBitmap.cs
@@ -1,5 +1,7 @@
 ﻿using IronSoftware.Drawing;
 using System;
 
+
+//Export AnyBitmap files to other format with ability to control loss
 AnyBitmap bitmap = new AnyBitmap(@"FILE_PATH");
-bitmap.ExportFile("losslogo.png", AnyBitmap.ImageFormat.Png, 100); //Export AnyBitmap file to other format with ability to control loss
+bitmap.ExportFile("losslogo.png", AnyBitmap.ImageFormat.Png, 100); 
