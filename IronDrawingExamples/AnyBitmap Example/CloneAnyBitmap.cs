5fb6fff4814f769dd25ff36531907585d5746232
Hairil Hasyimi Omar <115607157+hairil-ironsoftware@users.noreply.github.com>
2022-11-28 16:35:12 +0700

    Update CloneAnyBitmap.cs

--- IronDrawingExamples/AnyBitmap Example/CloneAnyBitmap.cs
+++ IronDrawingExamples/AnyBitmap Example/CloneAnyBitmap.cs
@@ -1,8 +1,10 @@
 ﻿using IronSoftware.Drawing;
 using System;
 
+// Clone AnyBitmap
 AnyBitmap clonedframe = AnyBitmap.FromFile(@"FILE_PATH").Clone();
-clonedframe.SaveAs("frameClone.jpg"); //clone AnyBitmap
+clonedframe.SaveAs("frameClone.jpg"); 
 
+//Clone frames of any image with a specified crop area
 AnyBitmap clonedWithCrop = AnyBitmap.FromFile(@"FILE_PATH").Clone(new CropRectangle(0, 0, 150, 150));
-clonedWithCrop.SaveAs("cropCloned.jpg"); //clone frames of any image with a specified crop area
+clonedWithCrop.SaveAs("cropCloned.jpg"); 
