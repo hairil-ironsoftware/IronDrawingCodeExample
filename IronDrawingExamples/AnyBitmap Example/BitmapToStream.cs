46f2faab2f215b3bb4e5722c7c579c69d7979cfe
Hairil Hasyimi Omar <115607157+hairil-ironsoftware@users.noreply.github.com>
2022-11-28 16:26:35 +0700

    Update BitmapToStream.cs

--- IronDrawingExamples/AnyBitmap Example/BitmapToStream.cs
+++ IronDrawingExamples/AnyBitmap Example/BitmapToStream.cs
@@ -2,5 +2,9 @@
 using System;
 
 AnyBitmap bitmap = AnyBitmap.FromFile(@"FILE_PATH");
-MemoryStream stream = bitmap.GetStream(); //get stream of an Anybitmap
-MemoryStream stream1 = bitmap.ToStream(); //convert AnyBitmap to stream 
+
+// Get stream of AnyBitmap
+MemoryStream stream = bitmap.GetStream();
+
+// Convert AnyBitmap to stream
+MemoryStream stream1 = bitmap.ToStream(); 
