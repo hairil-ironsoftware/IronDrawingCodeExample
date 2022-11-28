8a219e151e9165fbcd697955f93532f5b3cc3802
Hairil Hasyimi Omar <115607157+hairil-ironsoftware@users.noreply.github.com>
2022-11-28 16:45:49 +0700

    Update GetFrameFromAnyBitmap.cs

--- IronDrawingExamples/AnyBitmap Example/GetFrameFromAnyBitmap.cs
+++ IronDrawingExamples/AnyBitmap Example/GetFrameFromAnyBitmap.cs
@@ -6,7 +6,7 @@ multiPage.GetAllFrames.First().SaveAs("first.png"); //save first frame of a tiff
 multiPage.GetAllFrames.Last().SaveAs("last.png"); //save last frame of a tiff
 multiPage.GetAllFrames.ElementAt(1).SaveAs("second.png"); //save frame at an index of a tiff
 
-//save every frame of image of gif to image format
+// save every frame of image of gif to image format
 AnyBitmap multiFrameImage = AnyBitmap.FromFile(@"FILE_PATH.gif");
 List<AnyBitmap> frames = (List<AnyBitmap>)multiFrameImage.GetAllFrames;
 for (int i = 0; i < frames.Count; i++)
