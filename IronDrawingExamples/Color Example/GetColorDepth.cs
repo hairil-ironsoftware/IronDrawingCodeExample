3a7c3abe7a83f2f867071c21701908a1b3fbb82c
Hairil Hasyimi Omar <115607157+hairil-ironsoftware@users.noreply.github.com>
2022-12-01 13:25:44 +0700

    Update GetColorDepth.cs

--- IronDrawingExamples/Color Example/GetColorDepth.cs
+++ IronDrawingExamples/Color Example/GetColorDepth.cs
@@ -3,6 +3,6 @@
 //Load AnyBitmap sample via filepath
 AnyBitmap sample = AnyBitmap.FromFile("FILE_PATH");
 
-//Gets and print color dpeth in terms of bits per pixel
+//Gets and print color depth in terms of bits per pixel
 var colorDepth = sample.BitsPerPixel; 
 Console.WriteLine(colorDepth); 
