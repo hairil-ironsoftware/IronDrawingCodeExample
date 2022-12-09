9c551aa1a96b84c14d4eaaa73b8309ebbe4dc468
Hairil Hasyimi Omar <115607157+hairil-ironsoftware@users.noreply.github.com>
2022-12-09 16:53:16 +0700

    Update GetFrameFromAnyBitmap.cs

--- IronDrawingExamples/AnyBitmap Example/GetFrameFromAnyBitmap.cs
+++ IronDrawingExamples/AnyBitmap Example/GetFrameFromAnyBitmap.cs
@@ -1,4 +1,12 @@
-﻿using IronSoftware.Drawing;
+/**
+<h1> Get frame from AnyBitmap</h1>
+
+<p>This feature enable users to get or capture frames from a multipage GIF file or TIFF file and save it locally into any image format. Saved frames can be further manipulated in IronDrawing or other Iron Software tools as users wish.</p>
+
+<p>To use this feature, users must first convert the GIF or TIFF file into AnyBitmap type by loading it from a file path using <code>AnyBitmap.FromFile(@"FILE_PATH)</code> method. If users wanted to save individual frame and know the index number of the frame, <code>.GetAllFrames.ElementAt(n).SaveAs("FILE_NAME")</code> method can be used. To get the first and last frame, simply use <code>.First()</code> and <code>.Last()</code> methods accordingly. If users wish to save all frames and save it according to the frame number, users can use <code>for</code> loops on the frames saved in List and use <code>frames[i].SaveAs("frame " + i + ".jpg")</code>.</p>
+**/
+
+using IronSoftware.Drawing;
 
 //Load a GIF or TIFF as AnyBitmap (loads ALL frames)
 AnyBitmap gifOrTiff = AnyBitmap.FromFile(@"FILE_PATH");
