40f5e0210e9fe398a4da2bdcd78fae58007a6752
Hairil Hasyimi Omar <115607157+hairil-ironsoftware@users.noreply.github.com>
2022-12-09 15:45:00 +0700

    Update BitmapToString.cs

--- IronDrawingExamples/AnyBitmap Example/BitmapToString.cs
+++ IronDrawingExamples/AnyBitmap Example/BitmapToString.cs
@@ -1,4 +1,12 @@
-﻿using IronSoftware.Drawing;
+/**
+<h1> Convert AnyBitmap to string</h1>
+
+<p>IronDrawing also has a feature to convert AnyBitmap files to a unique string of characters that represent the current AnyBitmap files based on the characteristics of the AnyBitmap files. Any small change to a particular AnyBitmap file will change the string of the file and similar AnyBitmap file will have similar string. Therefore, it is useful as one way to check the similarity by comparing strings of two AnyBitmap files.</p>
+
+<p>To use this feature, simply load AnyBitmap file from a file path and assign it to a  **string**  variable to store the string of the AnyBitmap file loaded using the method <code>.ToString()</code> </p>
+**/
+
+using IronSoftware.Drawing;
 
 AnyBitmap bitmap = AnyBitmap.FromFile(@"FILE_PATH");
 
