39fbdfea1503e5f5ebf656e0026ebcef510ef452
Hairil Hasyimi Omar <115607157+hairil-ironsoftware@users.noreply.github.com>
2022-12-09 15:43:07 +0700

    Update BitmapToStream.cs

--- IronDrawingExamples/AnyBitmap Example/BitmapToStream.cs
+++ IronDrawingExamples/AnyBitmap Example/BitmapToStream.cs
@@ -1,4 +1,19 @@
-﻿using IronSoftware.Drawing;
+/**
+<h1> Converting Bitmap To Stream </h1>
+
+<p>AnyBitmap generated from a program or opened from a file directory in users computer can be converted to a memory stream to be used further in the program without the need to create a temporary buffers and files in an application. In this code example, we provide a simple way on how to get stream and also converting AnyBitmap file loaded from local directory to  memory stream. <p>
+
+<h2> Get Stream </h2>
+
+<p>This function is used to get the stream from a particular AnyBitmap file. Simply assign a MemoryStream variable to store the memory stream from an AnyBitmap file obtained using <code>.GetStream()</code> method.</p>
+
+<h2> To Stream </h2>
+
+<p>This function is used to convert AnyBitmap file to a memory stream. Simply assign a MemoryStream variable to store the value of AnyBitmap file converted to memory stream using <code>.ToStream()</code> method.</p>
+
+**/
+
+using IronSoftware.Drawing;
 
 AnyBitmap bitmap = AnyBitmap.FromFile(@"FILE_PATH");
 
