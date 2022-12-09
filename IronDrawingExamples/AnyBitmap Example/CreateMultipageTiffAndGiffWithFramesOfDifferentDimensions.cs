f31d53dfea840e031f85f8b2a802cf0412192fd1
Hairil Hasyimi Omar <115607157+hairil-ironsoftware@users.noreply.github.com>
2022-12-09 15:52:08 +0700

    Update CreateMultipageTiffAndGiffWithFramesOfDifferentDimensions.cs

--- IronDrawingExamples/AnyBitmap Example/CreateMultipageTiffAndGiffWithFramesOfDifferentDimensions.cs
+++ IronDrawingExamples/AnyBitmap Example/CreateMultipageTiffAndGiffWithFramesOfDifferentDimensions.cs
@@ -1,4 +1,12 @@
-﻿using IronSoftware.Drawing;
+/**
+<h1> Create Multipage TIFF and GIF </h1>
+
+<p>Users are able to combine AnyBitmap files into a multipage TIFF or GIF files in IronDrawing. Users can also use different dimensions of frame to create the multipage GIF and TIFF files.</p>
+
+<p>To use this feature, all AnyBitmap frames need to be loaded using the file path and stored inside an initialized List of AnyBitmap type. The TIFF file can now be created from the frames stored by using <code>.CreateMultiFrameTiff(List<AnyBitmap>)</code> or use <code>.CreateMultiFrameGif(List<AnyBitmap>)</code> to create GIF file.</p>
+**/
+
+using IronSoftware.Drawing;
 
 // Create a multipage Tiff with frames of different dimensions
 List<AnyBitmap> bitmaps = new List<AnyBitmap>()
