09f08c23625ad5e5e9b97f1312c3d1571e86f351
Hairil Hasyimi Omar <115607157+hairil-ironsoftware@users.noreply.github.com>
2022-12-12 14:40:09 +0700

    Update ReadBytesFromAnyBitmap.cs

--- IronDrawingExamples/AnyBitmap Example/ReadBytesFromAnyBitmap.cs
+++ IronDrawingExamples/AnyBitmap Example/ReadBytesFromAnyBitmap.cs
@@ -1,4 +1,12 @@
-﻿using IronSoftware.Drawing;
+/**
+<h1> Read bytes from AnyBitmap</h1>
+
+<p>Users have the option of manipulating AnyBitmap file in terms of bytes. IronDrawing have methods to **Get**, **Export**, and **Read** bytes of AnyBitmap file. </p>
+
+<p>The image file to be read can be loaded using file path <code>AnyBitmap.FromFile(@"FILE_PATH")</code> and stored in an AnyBitmap variable. The variable can then be manipulated to get bytes using <code>.GetBytes()</code> method, export bytes with option to change the image format and degree of image loss using <code>.ExportBytes(AnyBitmap.ImageFormat.Format, 10)</code> method and read all bytes of the file using <code>File.ReadAllBytes(@"FILE_PATH")</code> method.</p>
+**/
+
+using IronSoftware.Drawing;
 
 // Get, export, and read bytes
 AnyBitmap bitmap = AnyBitmap.FromFile(@"FILE_PATH");
